1ea4059f49d52bd352505e2d5888001abcf2ad2c
Alexandru Tabara <alexandru.tab1@gmail.com>
2022-11-27 14:55:10 +0200

    Add project files.

---
+++ lab1ex3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace lab1ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la tastaura
+
+            Console.WriteLine("Intoduceti un numar:");
+            int n = int.Parse(Console.ReadLine());
+
+            int ultimaCifra = n % 10;
+            Console.WriteLine("Ultima cifra a numarului introdus {0} este {1}", n, ultimaCifra);
+        }
+    }
+}
